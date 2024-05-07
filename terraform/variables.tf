1065231c540203d56a4636f82f3fc940c01aae7f
Mihail Mihaylov <mihail.georgiev.mihaylov@gmail.com>
2024-05-07 21:54:00 +0300

    Remove unneded terraform_state_bucket var
    This var was added to the init state but never used.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,3 @@
-variable "terraform_state_bucket" {
-  description = "The name of the S3 bucket to store Terraform state."
-  type        = string
-  default     = ""
-}
-
 variable "region" {
   description = "The name of the region in which the terraform S3 state bucket resides."
   type        = string
