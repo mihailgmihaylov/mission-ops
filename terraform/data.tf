59a71e48e9a2add960485222e848b9cc74d1440b
Mihail Mihaylov <mihail.georgiev.mihaylov@gmail.com>
2024-05-08 23:59:14 +0300

    Add S3 bucket and grant permissions to backend

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,5 +1,18 @@
 data "aws_region" "current" {}
+data "aws_partition" "current" {}
 
 data "http" "personal_public_ip_address" {
   url = "https://ifconfig.co/ip"
+}
+
+data "aws_iam_policy_document" "instance_assume_role" {
+  statement {
+    sid     = "InstanceAssumeRole"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["tasks.apprunner.${data.aws_partition.current.dns_suffix}"]
+    }
+  }
 }
