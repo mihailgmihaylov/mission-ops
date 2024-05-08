da0e76f3a74eca268ead2b25d21a1477176af06b
Mihail Mihaylov <mihail.georgiev.mihaylov@gmail.com>
2024-05-08 09:35:32 +0300

    Add RDS parameter group to log connections

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,3 +1,14 @@
+resource "aws_db_parameter_group" "log_access" {
+  name        = "${var.name}-db-param-group"
+  family      = "${var.database.engine}${regex("^[0-9]+", var.database.version)}"
+  description = "DB parameter group with read-only access"
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+}
+
 resource "aws_db_instance" "rds" {
   identifier             = "${var.name}-db"
   allocated_storage      = var.database.storage
@@ -10,6 +21,7 @@ resource "aws_db_instance" "rds" {
   username               = var.db_admin_user
   password               = var.db_admin_pass
   publicly_accessible    = var.database.is_public
+  parameter_group_name   = aws_db_parameter_group.log_access.name
   skip_final_snapshot    = true
   db_subnet_group_name   = aws_db_subnet_group.rds.name
   vpc_security_group_ids = [aws_security_group.rds_security_group.id]
